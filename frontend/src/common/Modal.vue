e6c961f6bb65e2fb86d0621046c17d5176d2b4dd
stivendiaz <stivendiazagudelo@gmail.com>
2023-04-19 11:02:53 -0500

    partial(landing): not finished modal and login form

--- frontend/src/common/Modal.vue
+++ frontend/src/common/Modal.vue
@@ -21,17 +21,17 @@
 <template>
   <div v-show="isOpen">
     <div
-      class="fixed inset-0 z-10"
+      class="fixed inset-0 z-10 bg-gray-500 bg-opacity-75"
       @click="$emit('closeModal')"
     ></div>
     <div
       class="inset-0 z-10 flex justify-center items-center"
     >
       <div
-        class="absolute bg-white rounded-2xl shadow-lg p-6 mx-4 md:max-w-screen-md top-[85%] right-[14%] animate-fade border-2 border-gray-200"
+        class="absolute top-10 bottom-10 overflow-y-auto z-20 bg-white rounded-2xl shadow-lg p-6 mx-4 md:max-w-screen-md animate-fade border-2 border-gray-200"
       >
-        <div class="flex justify-between items-center mb-4">
-          <h2 class="text-lg font-medium">{{ title }}</h2>
+        <div class="flex justify-between text-center items-center mb-4">
+          <h2 class="text-2xl font-bold sm:text-3xl">{{ title }}</h2>
           <button 
             class="hover:text-gray-700" 
             @click="$emit('closeModal')"
